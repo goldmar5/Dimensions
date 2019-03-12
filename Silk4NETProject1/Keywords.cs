8ce393078b4156847598f08d33dc105a80cb8986
goldmar5 <yuriysadovskiygold@gmail.com>
2019-03-12 14:22:17 +0200

    ss

--- Silk4NETProject1/Keywords.cs
+++ Silk4NETProject1/Keywords.cs
@@ -1,34 +1,11 @@
-using System;
-using System.Collections.Generic;
 using SilkTest.Ntf;
 using Silk.KeywordDriven;
-using SilkTest.Ntf.Wpf;
 
 public class Keywords
 {
-    private readonly Desktop _desktop = Agent.Desktop;
-
-    [Keyword("Start application", IsBaseState = true)]
-    public void Start_application(string Argument, bool UseRunDialog)
-    {
-        BaseState baseState = new BaseState();
-        baseState.Execute();
-    }
-
     [Keyword("TestKeyword")]
     public void TestKeyword()
     {
-        Agent.SystemFunctions.CreateDirectory("C:\\SILKCENTRAL");
-        //
-        WPFWindow dimensionsCMMicrosoftVisualStudioAdministrator = _desktop.WPFWindow("DimensionsCM - Microsoft Visual Studio  (Administrator)");
-        dimensionsCMMicrosoftVisualStudioAdministrator.WPFMenu("Menu_Bar").Select("/_View/Dimensions E_xplorer");
-        dimensionsCMMicrosoftVisualStudioAdministrator.Size(1554, 1139);
-        dimensionsCMMicrosoftVisualStudioAdministrator.Control("DevExpress XtraBars Controls DockedBarControl").TextClick("Connect...");
-
-        Dialog logInToDimensions = _desktop.Dialog("Log in to Dimensions");
-        logInToDimensions.TextField("txtPassword").SetPosition(new TextPosition(0, 0));
-        logInToDimensions.TextField("txtPassword").TypePasswordKeys("4U1tDNuSHZfC2w==");
-        logInToDimensions.PushButton("btnLogin").Select();
+        Agent.SystemFunctions.CreateDirectory("C:\\SILKCENTRALKDT");
     }
-
 }
--- Silk4NETProject1/UnitTest1.cs
+++ Silk4NETProject1/UnitTest1.cs
@@ -1,35 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SilkTest.Ntf;
-using SilkTest.Ntf.Wpf;
 
 namespace Silk4NETProject1
 {
     [SilkTestClass]
     public class UnitTest1
     {
-        private readonly Desktop _desktop = Agent.Desktop;
-
         [TestInitialize]
         public void Initialize()
         {
-            BaseState baseState = new BaseState();
-            baseState.Execute();
         }
 
         [TestMethod]
         public void TestMethod1()
         {
-            WPFWindow dimensionsCMMicrosoftVisualStudioAdministrator = _desktop.WPFWindow("DimensionsCM - Microsoft Visual Studio  (Administrator)");
-            dimensionsCMMicrosoftVisualStudioAdministrator.WPFMenu("Menu_Bar").Select("/_View/Dimensions E_xplorer");
-            dimensionsCMMicrosoftVisualStudioAdministrator.Size(1554, 1139);
-            dimensionsCMMicrosoftVisualStudioAdministrator.Control("DevExpress XtraBars Controls DockedBarControl").TextClick("Connect...");
-
-            Dialog logInToDimensions = _desktop.Dialog("Log in to Dimensions");
-            logInToDimensions.TextField("txtPassword").SetPosition(new TextPosition(0, 0));
-            logInToDimensions.TextField("txtPassword").TypePasswordKeys("4U1tDNuSHZfC2w==");
-            logInToDimensions.PushButton("btnLogin").Select();
+            Agent.SystemFunctions.CreateDirectory("C:\\SILKCENTRAL");
         }
     }
 }
